d189dae74234a548bea0385679ce4006c2032a78
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2023-10-19 17:12:12 -0300

    Commit Inicial - html estático

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './imagens/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="conteudo">
+      <img src={logo} alt='logo da calculadora de churrasco' />
+      <h1>Calculadora de Churrasco</h1>
+      <div id="painel">
+        <div class="painelDados">
+          <div class="painelControle">
+            <p>Homens:</p>
+            <span class="mais botao">+</span>
+            <span class="quantidade">1</span>
+            <span class="menos botao">-</span>
+          </div>
+          <div class="painelControle">
+            <p>Mulheres:</p>
+            <span class="mais botao">+</span>
+            <span class="quantidade">1</span>
+            <span class="menos botao">-</span>
+          </div>
+          <div class="painelControle">
+            <p>Crianças:</p>
+            <span class="mais botao">+</span>
+            <span class="quantidade">1</span>
+            <span class="menos botao">-</span>
+          </div>  
+        </div>
+        <div class="painelDados">
+          <div class="painelControle">
+            <p>Carne:</p>
+            <span class="quantidade">1</span>
+          </div>
+          <div class="painelControle">
+            <p>Pão:</p>
+            <span class="quantidade">1</span>
+          </div>
+          <div class="painelControle">
+            <p>Bebidas:</p>
+            <span class="quantidade">1</span>
+          </div> 
+        </div>
+      </div>
     </div>
   );
 }
